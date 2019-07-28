6f4eb5ff2597fdae945a49f55d9806d0599bdebc
Eric Cai <ecai@wustl.edu>
2019-07-28 14:41:15 +0200

    final (?) changes

--- scripts/first_model.R
+++ scripts/first_model.R
@@ -20,6 +20,9 @@ options(tibble.print_max = 30)
 df <- read.csv("data/raw/teaching_training_data.csv")
 
 
+barplot(table(df$numearnincome))
+
+
 # use readr::parse_number
 df <- df %>% 
   mutate(fin_situ_now = parse_number(as.character(financial_situation_now))) %>% 
@@ -31,90 +34,177 @@ df <- df %>%
 df <- df %>% mutate(working = as.factor(working))
 
 
-train <- filter(df, survey_num == 1) %>% # look only at the first survey
+# importing all of the test scores
+CFT <- read.csv("data/raw/teaching_training_data_cft.csv")
+COM <- read.csv("data/raw/teaching_training_data_com.csv")
+GRIT <- read.csv("data/raw/teaching_training_data_grit.csv")
+NUM <- read.csv("data/raw/teaching_training_data_num.csv")
+OPT <- read.csv("data/raw/teaching_training_data_opt.csv")
+
+
+# removing all NAs from the test scores
+CFT <- na.omit(CFT)
+COM <- na.omit(COM)
+GRIT <- na.omit(GRIT)
+NUM <- na.omit(NUM)
+OPT <- na.omit(OPT)
+
+# removing all duplicate entries based off of unique identifiers
+CFT <- distinct(CFT, unid, .keep_all = TRUE)
+COM <- distinct(COM, unid, .keep_all = TRUE)
+GRIT <- distinct(GRIT, unid, .keep_all = TRUE)
+NUM <- distinct(NUM, unid, .keep_all = TRUE)
+OPT <- distinct(OPT, unid, .keep_all = TRUE)
+
+
+df <- df %>% left_join(CFT, by = "unid") %>% select(-X.x, -X.y) %>% 
+  left_join(COM, by = "unid") %>% select(-X) %>% 
+  left_join(GRIT, by = "unid") %>% select(-X) %>% 
+  left_join(NUM, by = "unid") %>% select(-X) %>% 
+  left_join(OPT, by = "unid") %>% select(-X)
+
+
+
+df <- df %>% mutate(cft_score = as.factor(cft_score)) %>% 
+  mutate(com_score = as.factor(com_score)) %>% 
+  mutate(grit_score = as.factor(grit_score)) %>% 
+  mutate(num_score = as.factor(num_score)) %>% 
+  mutate(opt_score = as.factor(opt_score))
+
+
+
+
+#####
+data <- filter(df, survey_num == 1) %>% # look only at the first survey
   distinct(unid, .keep_all = TRUE) %>% # remove the duplicates for each unique identifier
   filter(unid != 17) # removed one entry where a woman entered survey date for her date of birth
 
 
 # calculating the baseline age and truncating to a whole number
-train <- train %>% mutate(age = interval(dob, as.Date(survey_date_month) %m-% months(4))/years(1)) %>% 
+data <- data %>% mutate(age = interval(dob, as.Date(survey_date_month) %m-% months(4))/years(1)) %>% 
   mutate(age = floor(age))
 
 
 # dropping irrelevant/redundant columns
-train <- train %>% select(-company_size, -survey_date_month, -job_start_date, -job_leave_date, -monthly_pay) %>% 
-  select(-financial_situation_5years, -financial_situation_now, -X, -survey_num, -dob, -fin_situ_change, -unid) %>% 
-  select(-peoplelive_15plus) # SHOW ANALYSIS OF THIS COLUMN - NO CORRELATION TO LIKELIHOOD OF WORKING
+data <- data %>% select(-company_size, -survey_date_month, -job_start_date, -job_leave_date, -monthly_pay) %>% 
+  select(-financial_situation_5years, -financial_situation_now, -survey_num, -dob, -fin_situ_change, -unid)
 
 # parsing numeric values out of responses for numearnincome, numchildren, peoplelive
-train <- train %>% mutate(numearnincome = parse_number(as.character(numearnincome)), 
+data <- data %>% mutate(numearnincome = parse_number(as.character(numearnincome)), 
                           numchildren = parse_number(as.character(numchildren)),
-                          peoplelive = parse_number(as.character(peoplelive)))
+                          peoplelive = as.numeric(parse_number(as.character(peoplelive))),
+                        peoplelive_15plus = as.numeric(parse_number(as.character(peoplelive_15plus))))
 
 
 
 # ---------- RUN THIS BLOCK TO EXAMINE NA TRENDS IN ALL OF THE FEATURES
-# total_na <- rowSums(is.na(train))
-# train_na <- cbind(train, total_na)
+total_na <- rowSums(is.na(data))
+# data <- data %>% cbind(total_na)
+data <- data %>% cbind(total_na) %>% mutate(total_na = as.factor(ifelse(total_na == 0, 0, 1)))
 
-# names(train_na)
+names(data)
 
-# examineNA <- mutate(train_na, work_true = ifelse(as.logical(working), 1, 0), work_false = ifelse(as.logical(working), 0, 1)) %>% 
-#   group_by(total_na) %>% summarise(total_true = sum(work_true), total_false = sum(work_false)) %>% 
-#   mutate(ratio = total_true/(total_false + total_true))
-# examineNA
+examineNA <- mutate(data, work_true = ifelse(as.logical(working), 1, 0), work_false = ifelse(as.logical(working), 0, 1)) %>% 
+ group_by(numearnincome) %>% summarise(total_true = sum(work_true), total_false = sum(work_false)) %>% 
+ mutate(ratio = total_true/(total_false + total_true))
+examineNA
+
+
+
+ggplot(data) +
+  aes(x = numearnincome) + 
+  geom_bar()
+
+
+ggplot(examineNA) + 
+  aes(x = numearnincome, y = ratio) + 
+  geom_point()
 # ---------------
-
 
 
 # INSERT ANALYSIS OF NA VALUES - IN PARTICULAR, INDIVIDUALS THAT DO NOT RESPOND TO SURVEY QUESTIONS SEEM TO BE MORE LIKELY
 # TO NOT WORK
 # FEATURES: province, volunteer, leadershiprole, peoplelive, numchildren, numearnincome, givemoney_yes, fin_situ_'s
-train <- train %>% mutate(province = as.factor(ifelse(is.na(province), "None", as.character(province)))) %>% 
+data <- data %>% mutate(province = as.factor(ifelse(is.na(province), "None", as.character(province)))) %>% 
   mutate(volunteer = as.factor(ifelse(is.na(volunteer), "None", as.character(volunteer)))) %>% 
-  mutate(leadershiprole = as.factor(ifelse(is.na(leadershiprole), "None", as.character(leadershiprole)))) %>% ###CUT OFF HERE
-  mutate(peoplelive = as.factor(ifelse(is.na(peoplelive), "None", as.character(peoplelive)))) %>%
-  mutate(numchildren = as.factor(ifelse(is.na(numchildren), "None", as.character(numchildren)))) %>%
-  mutate(numearnincome = as.factor(ifelse(is.na(numearnincome), "None", as.character(numearnincome)))) %>%
+  mutate(leadershiprole = as.factor(ifelse(is.na(leadershiprole), "None", as.character(leadershiprole)))) %>%
+  #mutate(peoplelive = as.factor(ifelse(is.na(peoplelive), "None", as.character(peoplelive)))) %>%
+  #mutate(peoplelive_15plus = as.factor(ifelse(is.na(peoplelive_15plus), "None", as.character(peoplelive_15plus)))) %>% 
+  #mutate(numchildren = as.factor(ifelse(is.na(numchildren), "None", as.character(numchildren)))) %>%
+  #mutate(numearnincome = as.factor(ifelse(is.na(numearnincome), "None", as.character(numearnincome)))) %>%
   mutate(givemoney_yes = as.factor(ifelse(is.na(givemoney_yes), "None", as.character(givemoney_yes)))) %>%
   mutate(fin_situ_now = as.factor(ifelse(is.na(fin_situ_now), "None", as.character(fin_situ_now)))) %>%
-  mutate(fin_situ_future = as.factor(ifelse(is.na(fin_situ_future), "None", as.character(fin_situ_future))))
+  mutate(fin_situ_future = as.factor(ifelse(is.na(fin_situ_future), "None", as.character(fin_situ_future)))) %>% 
+  mutate(cft_score = as.factor(ifelse(is.na(cft_score), "None", as.character(cft_score)))) %>% 
+  mutate(com_score = as.factor(ifelse(is.na(com_score), "None", as.character(com_score)))) %>% 
+  mutate(num_score = as.factor(ifelse(is.na(num_score), "None", as.character(num_score)))) %>% 
+  mutate(opt_score = as.factor(ifelse(is.na(opt_score), "None", as.character(opt_score))))
+
+
+data <- select(data, -grit_score)
 
 # ---- DOWNSIDE OF THIS: ALL OF THE FACTORS MUST BE TURNED INTO FACTORS ----
 # NUMERIC FEATURES THAT SHOULD BE CONSIDERED: peoplelive, numchildren, numearnincome, fin_situ's
 # CLUSTER/CATEGORIZE PEOPLELIVE, numearnincome
 
 
-# still need to use OPT, CPT, COM, etc.
+# still need to analyze OPT, CPT, COM, etc.
 
 
-as_tibble(train)
-skim_to_wide(train)
 
-names(train)
+
+
+
+skim_to_wide(data)
+
+
+
 
 
 set.seed(13)
-partition <- createDataPartition(train$working, p = 0.8, list = FALSE)
+partition <- createDataPartition(data$working, p = 0.8, list = FALSE)
 # sorry the naming is very poor here
-train.train <- train[partition, ]
-train.test <- train[-partition, ]
+train <- data[partition, ]
+test <- data[-partition, ]
 
 
+# UPSAMPLING TO BALANCE WORKING CLASSES
+train_employed = filter(train, working == TRUE)
+train_unemployed = filter(train, working == FALSE)
 
-# still need to try upsampling...I don't think ensemble methods do anything for us here
+nrow(train_employed)
+nrow(train_unemployed)
+difference <- nrow(train_unemployed) - nrow(train_employed)
+difference
+
+upsamples <- train_employed[sample(nrow(train_employed), nrow(train_employed), replace = TRUE),]
+
+train_employed <- rbind(train_employed, upsamples)
+train <- rbind(train, train_employed)
 
 
 trControl <- trainControl(method = "cv", number = 10, verboseIter = TRUE)
-model_tree <- train(working ~ ., train.train, method = "rpart", na.action = na.pass, 
+model_tree <- train(working ~ ., train, method = "rpart", na.action = na.pass, 
                     trControl = trControl, metric = "Kappa")
 
 
 
-predictions <- predict(model_tree, train.test, na.action = na.rpart)
+
+test_employed = filter(test, working == TRUE)
+test_unemployed = filter(test, working == FALSE)
+nrow(test_employed)
+nrow(test_unemployed)
+difference <- nrow(test_unemployed) - nrow(test_employed)
+difference
+
+
+
+
+
+predictions <- predict(model_tree, test, na.action = na.rpart)
 predictions
 
-hold <- table(predictions, train.test$working)
+hold <- table(predictions, test$working)
 confusionMatrix(hold)
 
 # VERY BAD SPECIFICITY - the model predicts FALSE too much, even when "working" is TRUE
@@ -127,3 +217,7 @@ confusionMatrix(hold)
 
 summary(model_tree$finalModel)
 modelLookup("rpart")
+
+
+names(data)
+skim_to_wide(data)
